1f102f26e7c3fe0abe919ba2b286669beaee8eb0
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-06-25 10:28:23 -0300

    Improved SignUpForm code

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,15 +1,86 @@
 import { useState, type SyntheticEvent } from "react";
 import { Link } from "react-router-dom";
 
-export default function SignInForm() {
+export default function SignUpForm() {
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [usernameError, setUsernameError] = useState<string>("");
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
 
-  const registerUser = async (event: SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const validateUsername = (username: string): boolean => {
+    if (!username) {
+      setUsernameError("Username is required");
+      return false;
+    }
 
-    const response = await fetch("http://localhost:8080/user/register", {
+    if (username.length < 3 || username.length > 50) {
+      setUsernameError("Username must be between 3 and 50 characters long");
+      return false;
+    }
+
+    return true;
+  };
+
+  const validateEmail = (email: string): boolean => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email) {
+      setEmailError("Email is required");
+      return false;
+    }
+
+    if (!regex.test(email)) {
+      setEmailError("Email must be valid");
+      return false;
+    }
+
+    return true;
+  };
+
+  const validatePassword = (password: string): boolean => {
+    if (!password) {
+      setPasswordError("Password is required");
+      return false;
+    }
+
+    if (password.length < 3 || password.length > 50) {
+      setPasswordError("Password must be between 3 and 50 characters long");
+      return false;
+    }
+
+    return true;
+  };
+
+  const validateFields = (
+    username: string,
+    email: string,
+    password: string,
+  ): boolean => {
+    let shouldRequest = true;
+
+    if (!validateUsername(username)) {
+      shouldRequest = false;
+    }
+
+    if (!validateEmail(email)) {
+      shouldRequest = false;
+    }
+
+    if (!validatePassword(password)) {
+      shouldRequest = false;
+    }
+
+    return shouldRequest;
+  };
+
+  const registerUser = async (
+    username: string,
+    email: string,
+    password: string,
+  ) => {
+    const response = await fetch("http://localhost:8080/users/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -18,11 +89,27 @@ export default function SignInForm() {
       body: JSON.stringify({ username, email, password }),
     });
 
-    if (!response.ok) {
-      return;
+    if (response.ok) {
+      return console.log("User has been successfully registered");
     }
 
-    console.log("User registered successfully");
+    const error = await response.json();
+
+    if (error.status === 409) {
+      setEmailError("Email already taken");
+    }
+  };
+
+  const handleRegisterFormSubmit = async (
+    event: SyntheticEvent<HTMLFormElement>,
+  ) => {
+    event.preventDefault();
+
+    const shouldRequest = validateFields(username, email, password);
+
+    if (!shouldRequest) return;
+
+    registerUser(username, email, password);
   };
 
   return (
@@ -33,49 +120,74 @@ export default function SignInForm() {
         <span className="text-center">The best plan makes the best day</span>
 
         <form
-          onSubmit={registerUser}
-          className="flex flex-col gap-4 w-full px-4"
+          onSubmit={handleRegisterFormSubmit}
+          className="flex flex-col gap-1 w-full px-4"
         >
-          <label htmlFor="username" className="flex flex-col gap-1">
-            Username *
-            <input
-              className="border bg-white text-black w-full placeholder-gray-500 rounded-sm p-2"
-              onChange={event => setUsername(event.target.value)}
-              value={username}
-              placeholder="Enter username"
-              type="text"
-              name="username"
-              id="username"
-            />
-          </label>
+          <div>
+            <label htmlFor="username" className="flex flex-col text-lg">
+              Username *
+              <input
+                className="border bg-white text-black w-full placeholder-gray-500 rounded-sm p-2"
+                onChange={event => {
+                  setUsername(event.target.value.trim());
+                  setUsernameError("");
+                }}
+                value={username}
+                placeholder="Enter username"
+                type="text"
+                name="username"
+                id="username"
+              />
+            </label>
 
-          <label htmlFor="email" className="flex flex-col gap-1">
-            Email *
-            <input
-              className="border bg-white text-black w-full placeholder-gray-500 rounded-sm p-2"
-              onChange={event => setEmail(event.target.value)}
-              value={email}
-              placeholder="Enter email"
-              type="email"
-              name="email"
-              id="email"
-            />
-          </label>
+            <span className="block text-red-400 text-sm whitespace-pre-wrap min-h-5">
+              {usernameError}
+            </span>
+          </div>
 
-          <label htmlFor="password" className="flex flex-col gap-1">
-            Password *
-            <input
-              className="border bg-white text-black w-full placeholder-gray-500 rounded-sm p-2"
-              onChange={event => setPassword(event.target.value)}
-              value={password}
-              placeholder="Enter password"
-              type="password"
-              name="password"
-              id="password"
-            />
-          </label>
+          <div>
+            <label htmlFor="email" className="flex flex-col text-lg">
+              Email *
+              <input
+                className="border bg-white text-black w-full placeholder-gray-500 rounded-sm p-2"
+                onChange={event => {
+                  setEmail(event.target.value.trim());
+                  setEmailError("");
+                }}
+                value={email}
+                placeholder="Enter email"
+                type="email"
+                name="email"
+                id="email"
+              />
+            </label>
+            <span className="block text-red-400 text-sm whitespace-pre-wrap min-h-5">
+              {emailError}
+            </span>
+          </div>
 
-          <div className="flex flex-col gap-3">
+          <div>
+            <label htmlFor="password" className="flex flex-col text-lg">
+              Password *
+              <input
+                className="border bg-white text-black w-full placeholder-gray-500 rounded-sm p-2"
+                onChange={event => {
+                  setPassword(event.target.value.trim());
+                  setPasswordError("");
+                }}
+                value={password}
+                placeholder="Enter password"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </label>
+            <span className="block text-red-400 text-sm whitespace-pre min-h-5">
+              {passwordError}
+            </span>
+          </div>
+
+          <div className="flex flex-col gap-3 mt-2">
             <button
               className="w-full cursor-pointer bg-black text-white font-bold rounded-sm p-3 transition-colors border-2 border-white"
               type="submit"
